44e4c05191a4e72323e70bcaf9fa5790b960f00a
avrillica <avrillica@yahoo.com>
2026-02-27 12:39:07 -0400

    Update config_flow.py

--- custom_components/nsp_energy_v2/config_flow.py
+++ custom_components/nsp_energy_v2/config_flow.py
@@ -8,7 +8,6 @@ class NSPConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="NSP Rates", data=user_input)
-
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
@@ -18,24 +17,16 @@ class NSPConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_INCLUDE_TAX, default=True): bool,
             })
         )
-
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
         return NSPOptionsFlowHandler(config_entry)
 
 class NSPOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
+    def __init__(self, config_entry): self.config_entry = config_entry
     async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            # This line fixes the 500 error by ensuring data is saved correctly
-            return self.async_create_entry(title="", data=user_input)
-        
-        # Pull from options first, then data, then defaults. This is the safety net.
+        if user_input is not None: return self.async_create_entry(title="", data=user_input)
         current = {**self.config_entry.data, **self.config_entry.options}
-        
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
