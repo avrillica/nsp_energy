379f5f06861f4fb7f196fc4fbe3f95615d0d9dc6
Nima <avrillica@yahoo.com>
2026-03-01 16:08:35 -0400

    Update __init__.py

--- custom_components/nsp_energy_v2/__init__.py
+++ custom_components/nsp_energy_v2/__init__.py
@@ -1,9 +1,23 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
-async def async_setup_entry(hass, entry):
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up NSP Energy from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    
+    # Register the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    
+    # Listen for changes to the Options menu
     entry.async_on_unload(entry.add_update_listener(update_listener))
+    
     return True
-async def update_listener(hass, entry):
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
-async def async_unload_entry(hass, entry):
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
