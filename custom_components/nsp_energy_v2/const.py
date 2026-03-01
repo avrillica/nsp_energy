387d153a75819e018ae4d885285c3018316cbb09
Nima <avrillica@yahoo.com>
2026-03-01 15:58:43 -0400

    Update const.py

--- custom_components/nsp_energy_v2/const.py
+++ custom_components/nsp_energy_v2/const.py
@@ -1,9 +1,11 @@
 DOMAIN = "nsp_energy_v2"
+
 CONF_PEAK_PRICE = "peak_price"
 CONF_MID_PRICE = "mid_price"
 CONF_OFFPEAK_PRICE = "offpeak_price"
 CONF_INCLUDE_TAX = "include_tax"
 
+# Official 2026 NSP TOD Rates (Pre-tax)
 DEFAULT_PEAK = 0.23821
 DEFAULT_MID = 0.19243
 DEFAULT_OFF = 0.11966
