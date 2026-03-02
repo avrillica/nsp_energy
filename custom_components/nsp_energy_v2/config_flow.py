0663f4c2a727272d42485679e978dcf529336427
Nima <avrillica@yahoo.com>
2026-03-02 17:14:50 -0400

    Update config_flow.py

--- custom_components/nsp_energy_v2/config_flow.py
+++ custom_components/nsp_energy_v2/config_flow.py
@@ -1,7 +1,11 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from .const import DOMAIN, CONF_PEAK_PRICE, CONF_MID_PRICE, CONF_OFFPEAK_PRICE, CONF_INCLUDE_TAX, DEFAULT_PEAK, DEFAULT_MID, DEFAULT_OFF
+from .const import (
+    DOMAIN, CONF_PEAK_PRICE, CONF_MID_PRICE, 
+    CONF_OFFPEAK_PRICE, CONF_INCLUDE_TAX, 
+    DEFAULT_PEAK, DEFAULT_MID, DEFAULT_OFF
+)
 
 class NSPEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -14,14 +18,19 @@ class NSPEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required(CONF_OFFPEAK_PRICE, default=DEFAULT_OFF): vol.Coerce(float),
             vol.Required(CONF_INCLUDE_TAX, default=True): bool,
         }))
+
     @staticmethod
     @callback
-    def async_get_options_flow(config_entry): return NSPEnergyOptionsFlowHandler(config_entry)
+    def async_get_options_flow(config_entry):
+        return NSPEnergyOptionsFlowHandler(config_entry)
 
 class NSPEnergyOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry): self.config_entry = config_entry
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
     async def async_step_init(self, user_input=None):
-        if user_input is not None: return self.async_create_entry(title="", data=user_input)
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
         return self.async_show_form(step_id="init", data_schema=vol.Schema({
             vol.Required(CONF_PEAK_PRICE, default=self.config_entry.options.get(CONF_PEAK_PRICE, DEFAULT_PEAK)): vol.Coerce(float),
             vol.Required(CONF_MID_PRICE, default=self.config_entry.options.get(CONF_MID_PRICE, DEFAULT_MID)): vol.Coerce(float),
